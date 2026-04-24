d207b473ca995c85d265b44ba284c6a3c09a9001
Sushma Sarkar <sushmasarkar.g@gmail.com>
2026-04-24 17:43:03 +0530

    added Eureka Server

--- src/main/java/com/sushma/olxadvertise/OlxAdvertiseApplication.java
+++ src/main/java/com/sushma/olxadvertise/OlxAdvertiseApplication.java
@@ -3,6 +3,7 @@ package com.sushma.olxadvertise;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OlxAdvertiseApplication {
 
 	public static void main(String[] args) {
